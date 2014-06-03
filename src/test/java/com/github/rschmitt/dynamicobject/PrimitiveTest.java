cff393a1a53cbf1f85c7af111d8fef0d1b98a1f5
Ryan Schmitt <rschmitt@pobox.com>
2014-06-02 23:39:23 -0700

    Extract PrimitiveTest from SmokeTest

---
+++ src/test/java/com/github/rschmitt/dynamicobject/PrimitiveTest.java
@@ -0,0 +1,77 @@
+package com.github.rschmitt.dynamicobject;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PrimitiveTest {
+    private static final String EDN = "{:i 4, :d 3.14, :f 3.14, :lng 1234567890, :shrt 4, :bool true}";
+    private static final Unboxed UNBOXED = DynamicObject.deserialize(EDN, Unboxed.class);
+    private static final Boxed BOXED = DynamicObject.deserialize(EDN, Boxed.class);
+
+    @Test
+    public void getBoxedFields() {
+        assertTrue(4 == BOXED.shrt());
+        assertTrue(4 == BOXED.i());
+        assertTrue(1234567890L == BOXED.lng());
+        assertEquals(3.14, BOXED.d(), 0.001);
+        assertEquals(3.14, BOXED.f(), 0.001);
+        assertTrue(BOXED.bool());
+    }
+
+    @Test
+    public void getUnboxedFields() {
+        assertTrue(4 == UNBOXED.shrt());
+        assertTrue(4 == UNBOXED.i());
+        assertTrue(1234567890L == UNBOXED.lng());
+        assertEquals(3.14, UNBOXED.d(), 0.001);
+        assertEquals(3.14, UNBOXED.f(), 0.001);
+        assertTrue(UNBOXED.bool());
+    }
+
+    @Test
+    public void unboxedRoundTrip() {
+        Unboxed unboxed = DynamicObject.deserialize(EDN, Unboxed.class);
+        assertEquals(EDN, unboxed.toString());
+    }
+
+    @Test
+    public void boxedRoundTrip() {
+        Boxed boxed = DynamicObject.deserialize(EDN, Boxed.class);
+        assertEquals(EDN, boxed.toString());
+    }
+
+    @Test
+    public void testEquality() {
+        assertEquals(BOXED, UNBOXED);
+    }
+
+    public interface Unboxed extends DynamicObject<Unboxed> {
+        short shrt();
+
+        int i();
+
+        long lng();
+
+        float f();
+
+        double d();
+
+        boolean bool();
+    }
+
+    public interface Boxed extends DynamicObject<Boxed> {
+        Short shrt();
+
+        Integer i();
+
+        Long lng();
+
+        Float f();
+
+        Double d();
+
+        Boolean bool();
+    }
+}
