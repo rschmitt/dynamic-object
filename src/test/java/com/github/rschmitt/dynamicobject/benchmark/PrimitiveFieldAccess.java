440916df7a1deb6cd92ecea1c02baf0e46f665c6
Ryan Schmitt <rschmitt@pobox.com>
2014-06-07 17:38:01 -0700

    Add some benchmarks for basic field access

---
+++ src/test/java/com/github/rschmitt/dynamicobject/benchmark/PrimitiveFieldAccess.java
@@ -0,0 +1,89 @@
+package com.github.rschmitt.dynamicobject.benchmark;
+
+import com.github.rschmitt.dynamicobject.DynamicObject;
+import org.junit.Test;
+
+public class PrimitiveFieldAccess {
+    private final int ITERATIONS = 500_000;
+
+    @Test
+
+    public void run() {
+        IntPojo intPojo = new IntPojo(1);
+        FinalIntPojo finalIntPojo = new FinalIntPojo(1);
+        IntField intField = DynamicObject.newInstance(IntField.class).i(1);
+
+        int acc = 0;
+        for (int i = 0; i < 15_000; i++) {
+            acc += intPojo.getI();
+            acc += finalIntPojo.getI();
+            acc += intField.i();
+        }
+        System.out.println(acc);
+
+        acc = 0;
+        long startTime = System.nanoTime();
+        for (int i = 0; i < ITERATIONS; i++) {
+            acc += intPojo.getI();
+        }
+        long endTime = System.nanoTime();
+        System.out.println(acc);
+        reportTime("intPojo", startTime, endTime);
+
+
+        acc = 0;
+        startTime = System.nanoTime();
+        for (int i = 0; i < ITERATIONS; i++) {
+            acc += finalIntPojo.getI();
+        }
+        endTime = System.nanoTime();
+        System.out.println(acc);
+        reportTime("finalIntPojo", startTime, endTime);
+
+
+        acc = 0;
+        startTime = System.nanoTime();
+        for (int i = 0; i < ITERATIONS; i++) {
+            acc += intField.i();
+        }
+        endTime = System.nanoTime();
+        System.out.println(acc);
+        reportTime("intField", startTime, endTime);
+    }
+
+    private void reportTime(String desc, long startTime, long endTime) {
+        long timeInMillis = (endTime - startTime) / 1000000;
+        System.out.println(String.format("%s: %,d ms", desc, timeInMillis));
+    }
+}
+
+
+interface IntField extends DynamicObject<IntField> {
+    int i();
+
+    IntField i(int i);
+}
+
+class IntPojo {
+    int i;
+
+    IntPojo(int i) {
+        this.i = i;
+    }
+
+    int getI() {
+        return i;
+    }
+}
+
+class FinalIntPojo {
+    private final int i;
+
+    FinalIntPojo(int i) {
+        this.i = i;
+    }
+
+    int getI() {
+        return i;
+    }
+}
---
+++ src/test/java/com/github/rschmitt/dynamicobject/benchmark/StringFieldAccess.java
@@ -0,0 +1,86 @@
+package com.github.rschmitt.dynamicobject.benchmark;
+
+import com.github.rschmitt.dynamicobject.DynamicObject;
+import org.junit.Test;
+
+public class StringFieldAccess {
+    private final int ITERATIONS = 500_000;
+
+    @Test
+    public void run() {
+        StringPojo stringPojo = new StringPojo("str");
+        FinalStringPojo finalStringPojo = new FinalStringPojo("str");
+        StringField stringField = DynamicObject.newInstance(StringField.class).str("str");
+
+        int acc = 0;
+        for (int i = 0; i < 15_000; i++) {
+            acc += stringPojo.str().length();
+            acc += finalStringPojo.str().length();
+            acc += stringField.str().length();
+        }
+
+        acc = 0;
+        long startTime = System.nanoTime();
+        for (int i = 0; i < ITERATIONS; i++) {
+            acc += stringPojo.str().length();
+        }
+        long endTime = System.nanoTime();
+        System.out.println(acc);
+        reportTime("stringPojo", startTime, endTime);
+
+
+        acc = 0;
+        startTime = System.nanoTime();
+        for (int i = 0; i < ITERATIONS; i++) {
+            acc += finalStringPojo.str().length();
+        }
+        endTime = System.nanoTime();
+        System.out.println(acc);
+        reportTime("finalStringPojo", startTime, endTime);
+
+
+        acc = 0;
+        startTime = System.nanoTime();
+        for (int i = 0; i < ITERATIONS; i++) {
+            acc += stringField.str().length();
+        }
+        endTime = System.nanoTime();
+        System.out.println(acc);
+        reportTime("stringField", startTime, endTime);
+    }
+
+    private void reportTime(String desc, long startTime, long endTime) {
+        long timeInMillis = (endTime - startTime) / 1000000;
+        System.out.println(String.format("%s: %,d ms", desc, timeInMillis));
+    }
+}
+
+interface StringField extends DynamicObject<StringField> {
+    String str();
+
+    StringField str(String str);
+}
+
+class StringPojo {
+    String str;
+
+    StringPojo(String str) {
+        this.str = str;
+    }
+
+    String str() {
+        return str;
+    }
+}
+
+class FinalStringPojo {
+    private final String str;
+
+    FinalStringPojo(String str) {
+        this.str = str;
+    }
+
+    String str() {
+        return str;
+    }
+}
