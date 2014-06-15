7694b61da0bd025afebea09c4fb03d903ab077e0
Ryan Schmitt <rschmitt@pobox.com>
2014-06-14 23:37:40 -0700

    Simplify RecordPrinter

--- src/main/java/com/github/rschmitt/dynamicobject/RecordPrinter.java
+++ src/main/java/com/github/rschmitt/dynamicobject/RecordPrinter.java
@@ -1,29 +1,18 @@
 package com.github.rschmitt.dynamicobject;
 
-import clojure.java.api.Clojure;
-import clojure.lang.IFn;
-
 import java.io.IOException;
 import java.io.Writer;
 
+import static com.github.rschmitt.dynamicobject.ClojureStuff.WITH_META;
 import static java.lang.String.format;
 
 public final class RecordPrinter {
-    private static final IFn WITH_META = Clojure.var("clojure.core", "with-meta");
-
-    private RecordPrinter() {
-    }
-
     /**
      * For use by print-method only. Do not call directly.
      */
-    public static Object printRecord(Object obj, Writer writer, String tag) {
+    public static Object printRecord(Object obj, Writer writer, String tag) throws IOException {
         obj = WITH_META.invoke(obj, null);
-        try {
-            writer.write(format("#%s%s", tag, obj.toString()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        writer.write(format("#%s%s", tag, obj.toString()));
         return null;
     }
 }
