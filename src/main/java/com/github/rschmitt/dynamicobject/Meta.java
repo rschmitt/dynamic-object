cdf011a2d54679867e9daac96b3e6f0e52e592ff
Ryan Schmitt <rschmitt@pobox.com>
2014-07-15 20:15:12 -0700

    Add JavaDoc for @Meta and @Required

--- src/main/java/com/github/rschmitt/dynamicobject/Meta.java
+++ src/main/java/com/github/rschmitt/dynamicobject/Meta.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Mark a field as metadata. Metadata is used to annotate a value with additional information that is not logically
+ * considered part of the value. Metadata is ignored for the purposes of equality and serialization.
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Meta {
--- src/main/java/com/github/rschmitt/dynamicobject/Required.java
+++ src/main/java/com/github/rschmitt/dynamicobject/Required.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a field as required. Fields marked with this annotation will throw a NullPointerException when accessed if a
+ * nonnull value is not present, or when the validate() method is called on the instance.
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Required {
