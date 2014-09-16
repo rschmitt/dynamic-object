268a6f15dcf94c95fe5d4d69922478abaa5cdcd9
Ryan Schmitt <rschmitt@pobox.com>
2014-09-15 19:10:29 -0700

    EdnTranslator: Remove redundant keywords

--- src/main/java/com/github/rschmitt/dynamicobject/EdnTranslator.java
+++ src/main/java/com/github/rschmitt/dynamicobject/EdnTranslator.java
@@ -4,15 +4,15 @@ public interface EdnTranslator<T> {
     /**
      * Read a tagged Edn object as its intended type.
      */
-    public abstract T read(Object obj);
+    T read(Object obj);
 
     /**
      * Return an Edn representation of the given object.
      */
-    public abstract String write(T obj);
+    String write(T obj);
 
     /**
      * Return the tag literal to use during serialization.
      */
-    public abstract String getTag();
+    String getTag();
 }
