c0aeeac34e3679a8c21e2db0e30a864e2d0e80cd
Ryan Schmitt <rschmitt@pobox.com>
2014-11-22 12:35:39 -0800

    ClojureStuff: Use static import

--- src/main/java/com/github/rschmitt/dynamicobject/ClojureStuff.java
+++ src/main/java/com/github/rschmitt/dynamicobject/ClojureStuff.java
@@ -1,50 +1,52 @@
 package com.github.rschmitt.dynamicobject;
 
-import clojure.java.api.Clojure;
 import clojure.lang.IFn;
 
+import static clojure.java.api.Clojure.read;
+import static clojure.java.api.Clojure.var;
+
 class ClojureStuff {
-    static final Object EmptyMap = Clojure.read("{}");
-    static final Object EmptySet = Clojure.read("#{}");
-    static final Object EmptyVector = Clojure.read("[]");
-    static final Object Type = Clojure.read(":type");
-    static final Object Readers = Clojure.read(":readers");
+    static final Object EmptyMap = read("{}");
+    static final Object EmptySet = read("#{}");
+    static final Object EmptyVector = read("[]");
+    static final Object Type = read(":type");
+    static final Object Readers = read(":readers");
 
-    static final IFn Assoc = Clojure.var("clojure.core", "assoc");
-    static final IFn AssocBang = Clojure.var("clojure.core", "assoc!");
-    static final IFn Bigint = Clojure.var("clojure.core", "bigint");
-    static final IFn Biginteger = Clojure.var("clojure.core", "biginteger");
-    static final IFn ConjBang = Clojure.var("clojure.core", "conj!");
-    static final IFn Deref = Clojure.var("clojure.core", "deref");
-    static final IFn Dissoc = Clojure.var("clojure.core", "dissoc");
-    static final IFn Eval = Clojure.var("clojure.core", "eval");
-    static final IFn Get = Clojure.var("clojure.core", "get");
-    static final IFn Memoize = Clojure.var("clojure.core", "memoize");
-    static final IFn MergeWith = Clojure.var("clojure.core", "merge-with");
-    static final IFn Meta = Clojure.var("clojure.core", "meta");
-    static final IFn Name = Clojure.var("clojure.core", "name");
-    static final IFn Nth = Clojure.var("clojure.core", "nth");
-    static final IFn Persistent = Clojure.var("clojure.core", "persistent!");
-    static final IFn PrintString = Clojure.var("clojure.core", "pr-str");
-    static final IFn Read = Clojure.var("clojure.edn", "read");
-    static final IFn ReadString = Clojure.var("clojure.edn", "read-string");
-    static final IFn RemoveMethod = Clojure.var("clojure.core", "remove-method");
-    static final IFn Transient = Clojure.var("clojure.core", "transient");
-    static final IFn WithMeta = Clojure.var("clojure.core", "with-meta");
-    static final IFn VaryMeta = Clojure.var("clojure.core", "vary-meta");
+    static final IFn Assoc = var("clojure.core", "assoc");
+    static final IFn AssocBang = var("clojure.core", "assoc!");
+    static final IFn Bigint = var("clojure.core", "bigint");
+    static final IFn Biginteger = var("clojure.core", "biginteger");
+    static final IFn ConjBang = var("clojure.core", "conj!");
+    static final IFn Deref = var("clojure.core", "deref");
+    static final IFn Dissoc = var("clojure.core", "dissoc");
+    static final IFn Eval = var("clojure.core", "eval");
+    static final IFn Get = var("clojure.core", "get");
+    static final IFn Memoize = var("clojure.core", "memoize");
+    static final IFn MergeWith = var("clojure.core", "merge-with");
+    static final IFn Meta = var("clojure.core", "meta");
+    static final IFn Name = var("clojure.core", "name");
+    static final IFn Nth = var("clojure.core", "nth");
+    static final IFn Persistent = var("clojure.core", "persistent!");
+    static final IFn PrintString = var("clojure.core", "pr-str");
+    static final IFn Read = var("clojure.edn", "read");
+    static final IFn ReadString = var("clojure.edn", "read-string");
+    static final IFn RemoveMethod = var("clojure.core", "remove-method");
+    static final IFn Transient = var("clojure.core", "transient");
+    static final IFn WithMeta = var("clojure.core", "with-meta");
+    static final IFn VaryMeta = var("clojure.core", "vary-meta");
 
-    static final Object PrintMethod = Deref.invoke(Clojure.var("clojure.core", "print-method"));
-    static final IFn CachedRead = (IFn) Memoize.invoke(Clojure.var("clojure.edn", "read-string"));
+    static final Object PrintMethod = Deref.invoke(var("clojure.core", "print-method"));
+    static final IFn CachedRead = (IFn) Memoize.invoke(var("clojure.edn", "read-string"));
     static final IFn Pprint;
     static final IFn Diff;
 
     static {
-        IFn require = Clojure.var("clojure.core", "require");
-        require.invoke(Clojure.read("clojure.pprint"));
-        require.invoke(Clojure.read("clojure.data"));
+        IFn require = var("clojure.core", "require");
+        require.invoke(read("clojure.pprint"));
+        require.invoke(read("clojure.data"));
 
-        Pprint = Clojure.var("clojure.pprint/pprint");
-        Diff = Clojure.var("clojure.data/diff");
+        Pprint = var("clojure.pprint/pprint");
+        Diff = var("clojure.data/diff");
     }
 
     static Object cachedRead(String edn) {
