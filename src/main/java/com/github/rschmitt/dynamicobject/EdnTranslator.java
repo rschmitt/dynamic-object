f42a4dc60506c7d935de24de0a295d2fe1c3a8f3
Ryan Schmitt <rschmitt@pobox.com>
2015-02-13 18:45:13 -0800

    EdnTranslator: Add Writer support
    This adds a new method, EdnTranslator#write(T, Writer), that permits
    unbuffered serialization directly to a writer. The default implementation
    is given in terms of the prior write method that returns a String; that
    method has also gained a default implementation that delegates to the
    Writer-based method. The idea here is that it should never be necessary to
    implement both methods.

--- src/main/java/com/github/rschmitt/dynamicobject/EdnTranslator.java
+++ src/main/java/com/github/rschmitt/dynamicobject/EdnTranslator.java
@@ -1,5 +1,9 @@
 package com.github.rschmitt.dynamicobject;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
 public interface EdnTranslator<T> {
     /**
      * Read a tagged Edn object as its intended type.
@@ -9,10 +13,25 @@ public interface EdnTranslator<T> {
     /**
      * Return an Edn representation of the given object.
      */
-    String write(T obj);
+    default String write(T obj) {
+        StringWriter stringWriter = new StringWriter();
+        write(obj, stringWriter);
+        return stringWriter.toString();
+    }
 
     /**
      * Return the tag literal to use during serialization.
      */
     String getTag();
+
+    /**
+     * Write an Edn representation of the given object to the given Writer.
+     */
+    default void write(T obj, Writer writer) {
+        try {
+            writer.write(write(obj));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
 }
