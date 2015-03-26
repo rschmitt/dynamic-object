cbd44c72d9bb2c7f8655244ac9441f23c0df1724
Ryan Schmitt <rschmitt@pobox.com>
2015-03-25 20:15:30 -0700

    Write dynamic-object instances as Fressian maps

--- src/main/java/com/github/rschmitt/dynamicobject/FressianReadHandler.java
+++ src/main/java/com/github/rschmitt/dynamicobject/FressianReadHandler.java
@@ -1,12 +1,6 @@
 package com.github.rschmitt.dynamicobject;
 
-import static com.github.rschmitt.dynamicobject.internal.ClojureStuff.AssocBang;
-import static com.github.rschmitt.dynamicobject.internal.ClojureStuff.EmptyMap;
-import static com.github.rschmitt.dynamicobject.internal.ClojureStuff.Persistent;
-import static com.github.rschmitt.dynamicobject.internal.ClojureStuff.Transient;
-
 import java.io.IOException;
-import java.util.List;
 
 import org.fressian.Reader;
 import org.fressian.handlers.ReadHandler;
@@ -20,11 +14,6 @@ public class FressianReadHandler implements ReadHandler {
 
     @Override
     public Object read(Reader r, Object tag, int componentCount) throws IOException {
-        Object result = Transient.invoke(EmptyMap);
-        List kvs = (List) r.readObject();
-        for (int i = 0; i < kvs.size(); i += 2) {
-            result = AssocBang.invoke(result, kvs.get(i), kvs.get(i + 1));
-        }
-        return DynamicObject.wrap(Persistent.invoke(result), type);
+        return DynamicObject.wrap(r.readObject(), type);
     }
 }
--- src/main/java/com/github/rschmitt/dynamicobject/FressianWriteHandler.java
+++ src/main/java/com/github/rschmitt/dynamicobject/FressianWriteHandler.java
@@ -1,9 +1,6 @@
 package com.github.rschmitt.dynamicobject;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import org.fressian.Writer;
 import org.fressian.handlers.WriteHandler;
@@ -21,12 +18,6 @@ public class FressianWriteHandler implements WriteHandler {
     @SuppressWarnings("unchecked")
     public void write(Writer w, Object instance) throws IOException {
         w.writeTag(tag, 1);
-        Map<Object, Object> map = ((DynamicObject) instance).getMap();
-        List<Object> kvs = new ArrayList<>(2 * map.size());
-        for (Map.Entry<Object, Object> entry : map.entrySet()) {
-            kvs.add(entry.getKey());
-            kvs.add(entry.getValue());
-        }
-        w.writeList(kvs);
+        w.writeObject(((DynamicObject) instance).getMap());
     }
 }
