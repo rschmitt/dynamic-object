089d69de79811b40be262f068b77c9950db94a90
Ryan Schmitt <rschmitt@pobox.com>
2015-06-10 15:30:53 -0700

    Improve Travis testing

---
+++ test-all-versions.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eux
+
+versions=(1.6.0 1.7.0-RC1 1.7.0-master-SNAPSHOT)
+
+for i in ${versions[@]}
+do
+    cp pom.xml pom-$i.xml
+    perl -i -pe 's/\[1.5.0,\)/'"$i"'/g' pom-$i.xml
+    mvn clean test -f pom-$i.xml
+done
+
+for i in ${versions[@]}
+do
+    rm pom-$i.xml
+done
