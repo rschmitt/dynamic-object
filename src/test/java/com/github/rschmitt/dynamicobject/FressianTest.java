3e0e912afffae1dedbe2e7f6e5a38212bbd188da
Bryan Donlan <bdonlan@gmail.com>
2015-11-05 23:54:27 -0800

    Add a fressian binary test vector

--- src/test/java/com/github/rschmitt/dynamicobject/FressianTest.java
+++ src/test/java/com/github/rschmitt/dynamicobject/FressianTest.java
@@ -6,7 +6,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Base64;
+
 public class FressianTest {
+
+    public static final BinarySerialized SAMPLE_VALUE
+            = DynamicObject.newInstance(BinarySerialized.class).withHello("world");
+
     @Before
     public void setup() {
         DynamicObject.registerTag(BinarySerialized.class, "BinarySerialized");
@@ -19,12 +25,20 @@ public class FressianTest {
 
     @Test
     public void smokeTest() throws Exception {
-        BinarySerialized binarySerialized = DynamicObject.newInstance(BinarySerialized.class).withHello("world");
+        BinarySerialized binarySerialized = SAMPLE_VALUE;
 
         byte[] bytes = DynamicObject.toFressianByteArray(binarySerialized);
         Object o = DynamicObject.fromFressianByteArray(bytes);
 
         assertEquals(o, binarySerialized);
+    }
+
+    @Test
+    public void formatCompatibilityTest() throws Exception {
+        String encoded = "7+MQQmluYXJ5U2VyaWFsaXplZAHA5sr3zd9oZWxsb993b3JsZA==";
+        BinarySerialized deserialized = DynamicObject.fromFressianByteArray(Base64.getDecoder().decode(encoded));
+
+        assertEquals(SAMPLE_VALUE, deserialized);
     }
 
     public interface BinarySerialized extends DynamicObject<BinarySerialized> {
