e2377208085d118c8fe7b815a0082cb382e522d8
Ryan Schmitt <rschmitt@pobox.com>
2015-11-08 17:16:31 -0800

    Change version to 1.6.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject dynamic-object "1.5.0-SNAPSHOT"
+(defproject dynamic-object "1.6.0-SNAPSHOT"
   :profiles {:dev
              {:dependencies
-              [[org.clojure/clojure "1.6.0"]
-               [com.github.rschmitt/collider "0.3.0"]
+              [[org.clojure/clojure "1.7.0"]
+               [com.github.rschmitt/collider "1.0.0-RC1"]
                [org.fressian/fressian "0.6.5"]
                [org.clojure/data.fressian "0.2.0"]
                [junit/junit "4.11"]
