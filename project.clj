d111af418de0a9f3e3df6366154c09aab0935711
Ryan Schmitt <ryansch@amazon.com>
2024-09-24 08:23:43 -0700

    Update Lein deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
              {:dependencies
               [[org.clojure/clojure "1.7.0"]
                [com.github.rschmitt/collider "1.0.0"]
-               [org.fressian/fressian "0.6.5"]
-               [org.clojure/data.fressian "0.2.0"]
-               [junit/junit "4.11"]
+               [org.fressian/fressian "0.6.8"]
+               [org.clojure/data.fressian "1.1.0"]
+               [junit/junit "4.13"]
                [org.ow2.asm/asm "7.1"]
                [collection-check  "0.1.6"]]}})
